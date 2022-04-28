01ec36a1a87a9e9e8b99afc3138cc75bdc35fcd6
Manideep <17101992m@gmail.com>
2022-04-29 01:39:10 +0530

    Photo enlarge completed

--- script.js
+++ script.js
@@ -1,11 +1,56 @@
 const photoContainer = document.querySelector('.photo-container')
 const images = document.querySelectorAll('.item')
+const gallery = document.querySelector('.gallery')
+const footer = document.querySelector('footer')
+const close1 = document.querySelectorAll('.close')
+// const close2 = document.querySelector('.close')
+const galImg = document.querySelectorAll('.galImg')
+const desc = document.querySelectorAll('.desc')
+
+// close2.addEventListener('click', () => {
+//     console.log('working')
+//     photoContainer.classList.add('hidden')
+// })
+
+close1.forEach(ele => {
+    ele.addEventListener('click', () => {
+        console.log('working');
+        ele.parentNode.classList.add('hidden')
+        // gallery.classList.remove('trans')
+        // footer.classList.remove('trans')
+    })
+})
+
+// images.forEach(item => {
+    
+// })
+
+
 
 
 images.forEach(image => {
     image.addEventListener('click', () => {
-        console.log("working")
-        photoContainer.classList.toggle('hidden')
+        galImg.forEach(img => {
+            img.style.width = "70vw";
+        })
+
+        close1.forEach(item => {
+            item.classList.remove('hidden')
+            
+        })
+
+        desc.forEach(item => {
+            item.classList.remove('hidden')
+        })
         photoContainer.appendChild(image)
+        photoContainer.classList.remove('hidden')
+        gallery.classList.add('trans')
+        footer.classList.add('trans')
     })
+
+    // document.querySelector('.close').addEventListener('click', () => {
+    //     console.log('working')
+    //     photoContainer.classList.add('hidden')
+    //     gallery.classList.remove('trans')
+    // })
 })
