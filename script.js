38a97cd58733f8e6e7747589ce479109059afe20
Manideep <17101992m@gmail.com>
2022-04-30 01:09:51 +0530

    desktop version completed

--- script.js
+++ script.js
@@ -1,56 +1,30 @@
-const photoContainer = document.querySelector('.photo-container')
-const images = document.querySelectorAll('.item')
-const gallery = document.querySelector('.gallery')
-const footer = document.querySelector('footer')
-const close1 = document.querySelectorAll('.close')
-// const close2 = document.querySelector('.close')
-const galImg = document.querySelectorAll('.galImg')
-const desc = document.querySelectorAll('.desc')
-
-// close2.addEventListener('click', () => {
-//     console.log('working')
-//     photoContainer.classList.add('hidden')
-// })
-
-close1.forEach(ele => {
-    ele.addEventListener('click', () => {
-        console.log('working');
-        ele.parentNode.classList.add('hidden')
-        // gallery.classList.remove('trans')
-        // footer.classList.remove('trans')
-    })
-})
-
-// images.forEach(item => {
-    
-// })
+const photoContainer = document.querySelector(".photo-container");
+const contents = document.querySelector(".contents");
+const gallery = document.querySelector(".gallery");
+const footer = document.querySelector("footer");
+const photoHeading = document.querySelectorAll(".photoHeading");
+const close = document.querySelector(".close");
+const galImg = document.querySelectorAll(".galImg");
+const para = document.querySelector(".heading");
+let desc = undefined;
 
 
 
+close.addEventListener("click", () => {
+  photoContainer.classList.add("hidden");
+  para.removeChild(desc);
+  gallery.classList.remove("trans");
+  footer.classList.remove("trans");
+});
 
-images.forEach(image => {
-    image.addEventListener('click', () => {
-        galImg.forEach(img => {
-            img.style.width = "70vw";
-        })
+galImg.forEach((img) => {
+  img.addEventListener("click", () => {
+    contents.style.background = `url('${img.src}') no-repeat center center/cover`;
+    desc = document.createTextNode(`${img.alt}`);
+    para.appendChild(desc);
+    photoContainer.classList.remove("hidden");
+    gallery.classList.add("trans");
+    footer.classList.add("trans");
+  });
+});
 
-        close1.forEach(item => {
-            item.classList.remove('hidden')
-            
-        })
-
-        desc.forEach(item => {
-            item.classList.remove('hidden')
-        })
-        photoContainer.appendChild(image)
-        photoContainer.classList.remove('hidden')
-        gallery.classList.add('trans')
-        footer.classList.add('trans')
-    })
-
-    // document.querySelector('.close').addEventListener('click', () => {
-    //     console.log('working')
-    //     photoContainer.classList.add('hidden')
-    //     gallery.classList.remove('trans')
-    // })
-})
